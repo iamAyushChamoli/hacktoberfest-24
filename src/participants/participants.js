7cc0ec071d43284bb28fe0ebe418f84e931de50d
unknown <ayushcodingprofile@gmail.com>
2024-10-01 14:08:32 +0530

    TEST ISSUE + PR

--- src/participants/participants.js
+++ src/participants/participants.js
@@ -3,10 +3,10 @@ import { ayush } from "./avatars/index";
 export const participantData = [
   {
     id: "1",
-    name: "Ayush ChamoliK1",
+    name: "Ayush Chamoli TEST",
     university: "Graphic Era University",
     img: ayush,
     pr: [1],
-    prLink: ['#link1'],
+    prLink: ['https://github.com/iamAyushChamoli/hacktoberfest-24/pull/2'],
   },
 ]
