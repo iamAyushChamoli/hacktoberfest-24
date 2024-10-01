dbe70b4422ac361008f39b779d5fce362508d006
Akhand Pratap Singh <akhandpratapsingh1817@gmail.com>
2024-10-01 15:57:42 +0530

    Update participants.js

--- src/participants/participants.js
+++ src/participants/participants.js
@@ -9,4 +9,13 @@ export const participantData = [
     pr: [1],
     prLink: ['#link1'],
   },
+
+  export const participantData = [
+  {
+    id: "2",
+    name: "Akhand Pratap Singh",
+    university: "Graphic Era University",
+    pr: [2],
+    prLink: ['https://github.com/iamAyushChamoli/hacktoberfest-24/pull/13#issuecomment-2385340783'],
+  },
 ]
