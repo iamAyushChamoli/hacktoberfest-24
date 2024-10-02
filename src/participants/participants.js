7bcd090e35d5edeca77bd1b33abbdebe357679c3
uday1508 <udayg256@gmail.com>
2024-10-02 13:11:21 +0530

    [FIXED]:Test Date Was Removed From participants.js file

--- src/participants/participants.js
+++ src/participants/participants.js
@@ -1,23 +1,7 @@
 import { ayush, vineet } from "./avatars/index";
 
 export const participantData = [
-  {
-    id: "1",
-    name: "Ayush ChamoliK1",
-    university: "Graphic Era University",
-    img: ayush,
-    pr: [1],
-    prLink: ["#link1"],
-  },
-  {
-    id: "2",
-    name: "test",
-    university: "Graphic Era University",
-    img: ayush,
-    pr: [1, 123],
-    prLink: ["ink1", "link2"],
-  },
-  {
+ {
     id: "3",
     name: "Vineet Singh",
     university: "Graphic Era Hill University",
