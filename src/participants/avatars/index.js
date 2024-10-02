b4022e9301a010296232838f349a83ea1cafa556
Hayagreevan V <hayagree@gmail.com>
2024-10-02 18:25:01 +0530

    updating participants.js

--- src/participants/avatars/index.js
+++ src/participants/avatars/index.js
@@ -1,5 +1,6 @@
 import ayush from "./img/ayush.jpg";
 import vineet from "./img/vineet.jpg";
+import hayagreevanv from "./img/HayagreevanV.png"
 
-export { ayush, vineet };
+export { ayush, vineet, hayagreevanv };
 
--- src/participants/participants.js
+++ src/participants/participants.js
@@ -1,4 +1,5 @@
 import { vineet } from "./avatars/index";
+import { hayagreevanv } from "./avatars/index";
 
 export const participantData = [
  {
@@ -9,4 +10,12 @@ export const participantData = [
     pr: [25],
     prLink: ["https://github.com/iamAyushChamoli/hacktoberfest-24/pull/25"],
   },
+  {
+    id: "2",
+    name: "Hayagreevan V",
+    university: "Rajalakshmi Engineering College",
+    img: hayagreevanv,
+    pr: [28],
+    prLink: ['https://github.com/iamAyushChamoli/hacktoberfest-24/pull/28'],
+},
 ];
