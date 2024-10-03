17db4ed975a4e92e2ec82e4c80521f56ed360b23
Mohit Rana <mohitr8998@gmail.com>
2024-10-03 09:07:05 -0400

    solve some conflicts

--- src/participants/avatars/index.js
+++ src/participants/avatars/index.js
@@ -2,6 +2,5 @@ import ayush from "./img/ayush.jpg";
 import vineet from "./img/vineet.jpg";
 import hayagreevanv from "./img/HayagreevanV.png"
 import uday from "./img/uday.png"
-
+import umesh from "./img/umesh.png"
 export { ayush, vineet, hayagreevanv, uday , umesh };
-
