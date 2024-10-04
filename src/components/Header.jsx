49206fdec3c101af9460b00e7290ec9582665ab4
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2024-10-04 15:06:56 +0530

    Navsection fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[7rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
