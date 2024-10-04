8080727b73057d19797e8ac4ba7a0f3d05fc8abe
Tanuj <d3p8maxp@duck.com>
2024-10-04 23:47:29 +0530

    add tanujlord in participants.js

--- src/participants/avatars/index.js
+++ src/participants/avatars/index.js
@@ -4,6 +4,7 @@ import hayagreevanv from "./img/HayagreevanV.png"
 import uday from "./img/uday.png"
 import umesh from "./img/umesh.png"
 import mohit from "./img/mohit.jpg"
+import tanujlord from "./img/tanujlord.jpg"
 
 import divyanshu from "./img/divyanshu.png"
-export { ayush, vineet, hayagreevanv, uday , umesh, mohit ,divyanshu };
+export { ayush, vineet, hayagreevanv, uday , umesh, mohit ,divyanshu, tanujlord };
