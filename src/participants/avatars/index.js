01160d71bc3708ed259135d6d498fcdf1a5f3817
ayushi gaur <107021265+gaurayushi@users.noreply.github.com>
2024-10-05 21:32:33 +0530

    Delete src/participants/avatars/index.js

--- src/participants/avatars/index.js
+++
@@ -1,8 +0,0 @@
-import ayush from "./img/ayush.jpg";
-import vineet from "./img/vineet.jpg";
-import hayagreevanv from "./img/HayagreevanV.png"
-import uday from "./img/uday.png"
-import umesh from "./img/umesh.png"
-import mohit from "./img/mohit.jpg"
-import divyanshu from "./img/divyanshu.png"
-export { ayush, vineet, hayagreevanv, uday , umesh, mohit ,divyanshu };
