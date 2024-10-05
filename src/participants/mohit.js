85babba1af371232962c6b262b354f3569eadc3e
Rishaub Kumar <rishaubkumar534@gmail.com>
2024-10-06 01:21:43 +0530

    participants.js file merge conflict resolved

---
+++ src/participants/mohit.js
@@ -0,0 +1,15 @@
+import mohitImg from "./avatars/img/mohit.jpg";
+
+const mohit = {
+  id: "5",
+  name: "Mohit Rana",
+  university: "Graphic Era Hill University",
+  img: mohitImg,
+  pr: [45, 54],
+  prLink: [
+    'https://github.com/iamAyushChamoli/hacktoberfest-24/pull/50',
+    'https://github.com/aditya-bhaumik/Pathsphere/pull/114'
+  ]
+};
+
+export default mohit;
---
+++ src/participants/umesh.js
@@ -0,0 +1,17 @@
+import umeshImg from "./avatars/img/umesh.png";
+
+const umesh = {
+  id: "4",
+  name: "Umesh Kumar",
+  university: "Graphic Era Hill University",
+  img: umeshImg,
+  pr: [37, 30, 35, 41],
+  prLink: [
+    'https://github.com/iamAyushChamoli/hacktoberfest-24/pull/37',
+    'https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/30',
+    'https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/35',
+    'https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/41'
+  ]
+};
+
+export default umesh;
