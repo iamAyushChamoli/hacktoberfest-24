73d92bbc46092b024e84e7b43d6b233dc5dd2c57
Rishaub Kumar <rishaubkumar534@gmail.com>
2024-10-06 02:21:13 +0530

    final edit Explore.md

---
+++ src/participants/avatars/index.js
@@ -0,0 +1,11 @@
+import ayush from "./img/ayush.jpg";
+import vineet from "./img/vineet.jpg";
+import hayagreevanv from "./img/HayagreevanV.png"
+import uday from "./img/uday.png"
+import umesh from "./img/umesh.png"
+import mohit from "./img/mohit.jpg"
+import pratikM from './img/pratikM.jpg'
+import divyanshu from "./img/divyanshu.png"
+import tanuj from "./img/tanuj.jpg"
+
+export { ayush, vineet, hayagreevanv, uday , umesh, mohit ,divyanshu,pratikM, tanuj};
