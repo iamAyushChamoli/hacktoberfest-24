80334acc40b99ca4581472ef4deb412d49a9371f
Rishaub Kumar <rishaubkumar534@gmail.com>
2024-10-06 11:59:24 +0530

    participant-conflict-fixed

---
+++ src/participants/divyanshu.js
@@ -0,0 +1,12 @@
+import divyanshuImg from "./avatars/img/divyanshu.png";
+
+const divyanshu = {
+  id: "6",
+  name: "Divyanshu Chaurasiya",
+  university: "Graphic Era Hill University",
+  img: divyanshuImg,
+  pr: [60],
+  prLink: ['https://github.com/iamAyushChamoli/hacktoberfest-24/pull/60']
+};
+
+export default divyanshu;
---
+++ src/participants/pratikM.js
@@ -0,0 +1,12 @@
+import pratikMImg from "./avatars/img/pratikM.jpg";
+
+const pratikM = {
+  id: "7",
+  name: "Pratik Mali",
+  university: "MET Institute of Engineering",
+  img: pratikMImg,
+  pr: [61],
+  prLink: ['https://github.com/iamAyushChamoli/hacktoberfest-24/pull/61']
+};
+
+export default pratikM;
