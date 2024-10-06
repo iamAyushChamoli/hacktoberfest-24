03ef0d9edb442ee01e44f94845e001c8ba648dfb
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-10-06 21:38:18 +0530

    Updated Umesh.js file

--- src/participants/umesh.js
+++ src/participants/umesh.js
@@ -5,13 +5,8 @@ const umesh = {
   name: "Umesh Kumar",
   university: "Graphic Era Hill University",
   img: umeshImg,
-  pr: [37, 30, 35, 41],
-  prLink: [
-    'https://github.com/iamAyushChamoli/hacktoberfest-24/pull/37',
-    'https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/30',
-    'https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/35',
-    'https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/41'
-  ]
+  pr: [37,30,35,41,64,61,81],
+  prLink: ['https://github.com/iamAyushChamoli/hacktoberfest-24/pull/37','https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/30','https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/35','https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/41','https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/64','https://github.com/AkshitLakhera/PenCraft-Full-Stack-Blogging-Application/pull/61','https://github.com/iamAyushChamoli/hacktoberfest-24/pull/81']
 };
 
 export default umesh;
