68cde2de750addd61c0411f77169e6011fd9e2a9
Ashish Kumar Sahoo <79834663+ashish5kmax@users.noreply.github.com>
2024-10-14 23:48:31 +0530

    Update ashish_kumar_sahoo.js
    * Done 2 more contributions in open source recently. 
    
    * Thank you.

--- src/participants/ashish_kumar_sahoo.js
+++ src/participants/ashish_kumar_sahoo.js
@@ -5,12 +5,14 @@ const ashish_kumar_sahoo = {
     name: "Ashish Kumar Sahoo",
     university: "BMS Institute of Technology and Management",
     img: ashishImg,
-    pr: [75, 64, 77, 84],
+    pr: [75, 64, 77, 84, 65, 2761],
     prLink: [
         'https://github.com/handikatriarlan/hello-world-in-every-language/pull/75',
         'https://github.com/handikatriarlan/hello-world-in-every-language/pull/64',
         'https://github.com/handikatriarlan/hello-world-in-every-language/pull/77',
-        'https://github.com/ghostmkg/programming-language/pull/84'
+        'https://github.com/ghostmkg/programming-language/pull/84',
+        'https://github.com/Pradeepsingh61/DSA_Code/pull/65',
+        'https://github.com/TheAlgorithms/C-Plus-Plus/pull/2761',
     ]
 };
 
