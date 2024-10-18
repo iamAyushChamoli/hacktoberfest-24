69e3b4d20b35c830ba0e74d74c3b84dfc127095a
Mohit Rana <mohitr8998@gmail.com>
2024-10-18 09:28:11 -0400

    added new pr in participant of mohit rana

--- src/participants/mohit.js
+++ src/participants/mohit.js
@@ -5,13 +5,14 @@ const mohit = {
   name: "Mohit Rana",
   university: "Graphic Era Hill University",
   img: mohitImg,
-  pr: [50, 114, 183, 69, 128],
+  pr: [50, 114, 183, 69, 128, 691],
   prLink: [
     'https://github.com/iamAyushChamoli/hacktoberfest-24/pull/50',
     'https://github.com/aditya-bhaumik/Pathsphere/pull/114',
     'https://github.com/GarimaSingh0109/Resum-Resume/pull/183',
     'https://github.com/ML-Fusion-Lab/ML-Fusion-Lab-Website/pull/69',
-    'https://github.com/RamakrushnaBiswal/PlayCafe/pull/128'
+    'https://github.com/RamakrushnaBiswal/PlayCafe/pull/128',
+    'https://github.com/aditya-bhaumik/Pathsphere/pull/691'
   ]
 };
 
